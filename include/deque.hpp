a2c6c5d62b0b2148dcf702d6061534a6d1e3fa97
mylastresort <samytamim42@gmail.com>
2024-09-17 14:19:24 +0100

    refactoring deque member typedefs and declaring deque_iterator class members

--- include/deque.hpp
+++ include/deque.hpp
@@ -5,23 +5,32 @@
 
 namespace ft
 {
-template < class T, class Allocator = std::allocator< T > > class deque
+
+template < class T > struct deque_iterator;
+
+template < class T, class Alloc = std::allocator< T > > class deque
 {
   public:
-	typedef Allocator allocator_type;
-	typedef const iterator< random_access_iterator_tag, T > const_iterator;
-	typedef const T* const_pointer;
-	typedef const T& const_reference;
-	typedef iterator< random_access_iterator_tag, T > iterator;
+	typedef Alloc allocator_type;
 	typedef ptrdiff_t difference_type;
-	typedef reverse_iterator< const_iterator > const_reverse_iterator;
-	typedef reverse_iterator< iterator > reverse_iterator;
 	typedef size_t size_type;
 	typedef T value_type;
-	typedef T* pointer;
-	typedef T& reference;
 
-	// constructors
+	typedef typename allocator_type::const_pointer const_pointer;
+	typedef typename allocator_type::const_reference const_reference;
+	typedef typename allocator_type::pointer pointer;
+	typedef typename allocator_type::reference reference;
+
+	typedef deque_iterator< const value_type > const_iterator;
+	typedef deque_iterator< value_type > iterator;
+
+	typedef ft::reverse_iterator< const_iterator > const_reverse_iterator;
+	typedef ft::reverse_iterator< iterator > reverse_iterator;
+
+  private:
+	allocator_type alloc;
+
+  public:
 	explicit deque(const allocator_type& = allocator_type());
 	explicit deque(size_type size,
 				   const value_type& value = value_type(),
@@ -91,27 +100,73 @@ template < class T, class Allocator = std::allocator< T > > class deque
 	void swap(deque& other);
 };
 
-template < class T, class Allocator >
-bool operator==(const deque< T, Allocator >& lhs,
-				const deque< T, Allocator >& rhs);
-template < class T, class Allocator >
-bool operator<(const deque< T, Allocator >& lhs,
-			   const deque< T, Allocator >& rhs);
-template < class T, class Allocator >
-bool operator!=(const deque< T, Allocator >& lhs,
-				const deque< T, Allocator >& rhs);
-template < class T, class Allocator >
-bool operator>(const deque< T, Allocator >& lhs,
-			   const deque< T, Allocator >& rhs);
-template < class T, class Allocator >
-bool operator>=(const deque< T, Allocator >& lhs,
-				const deque< T, Allocator >& rhs);
-template < class T, class Allocator >
-bool operator<=(const deque< T, Allocator >& lhs,
-				const deque< T, Allocator >& rhs);
+template < class T, class Alloc >
+bool operator==(const deque< T, Alloc >& lhs, const deque< T, Alloc >& rhs);
+template < class T, class Alloc >
+bool operator<(const deque< T, Alloc >& lhs, const deque< T, Alloc >& rhs);
+template < class T, class Alloc >
+bool operator!=(const deque< T, Alloc >& lhs, const deque< T, Alloc >& rhs);
+template < class T, class Alloc >
+bool operator>(const deque< T, Alloc >& lhs, const deque< T, Alloc >& rhs);
+template < class T, class Alloc >
+bool operator>=(const deque< T, Alloc >& lhs, const deque< T, Alloc >& rhs);
+template < class T, class Alloc >
+bool operator<=(const deque< T, Alloc >& lhs, const deque< T, Alloc >& rhs);
 
-template < class T, class Allocator >
-void swap(deque< T, Allocator >& first, deque< T, Allocator >& second);
+template < class T, class Alloc >
+void swap(deque< T, Alloc >& first, deque< T, Alloc >& second);
+
+template < class T >
+struct deque_iterator : public ft::iterator< random_access_iterator_tag, T >
+{
+	typedef ft::iterator< random_access_iterator_tag, T > _iterator;
+
+	using typename _iterator::difference_type;
+	using typename _iterator::iterator_category;
+	using typename _iterator::pointer;
+	using typename _iterator::reference;
+	using typename _iterator::value_type;
+
+	deque_iterator();
+	deque_iterator(const deque_iterator& cpy);
+	deque_iterator& operator=(const deque_iterator& cpy);
+	~deque_iterator();
+
+	explicit deque_iterator(pointer _current);
+
+	bool operator!=(const deque_iterator& rhs);
+	bool operator<(const deque_iterator& rhs);
+	bool operator<=(const deque_iterator& rhs);
+	bool operator==(const deque_iterator& rhs);
+	bool operator>(const deque_iterator& rhs);
+	bool operator>=(const deque_iterator& rhs);
+	const pointer operator->() const;
+	const reference operator[](int post) const;
+	const reference operator*() const;
+	pointer base() const;
+	pointer operator->();
+	reference operator[](int pos);
+	reference operator*();
+	template < class U >
+	difference_type operator-(const deque_iterator< U >& rhs);
+	deque_iterator operator--(int); // NOLINT
+	deque_iterator operator-(const difference_type n);
+	deque_iterator operator+(const difference_type n);
+	deque_iterator operator++(int); // NOLINT
+	deque_iterator& operator--();
+	deque_iterator& operator-=(const difference_type n);
+	deque_iterator& operator++();
+	deque_iterator& operator+=(const difference_type n);
+
+  private:
+	pointer current;
+};
+
+template < class T >
+deque_iterator< T >&
+operator+(const typename deque_iterator< T >::difference_type lhs,
+		  deque_iterator< T >& rhs);
+
 } // namespace ft
 
 #include "templates/deque.tpp" // IWYU pragma: export
