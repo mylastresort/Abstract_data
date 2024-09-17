69cf6978f8920445f8747f7b98189603a0f1f017
mylastresort <samytamim42@gmail.com>
2024-09-17 12:15:09 +0100

    adding type_traits.hpp

---
+++ include/type_traits.hpp
@@ -0,0 +1,21 @@
+#ifndef TYPES_TRAITS_HPP
+#define TYPES_TRAITS_HPP
+
+#include <limits>
+namespace ft
+{
+
+template < bool B, class T = void > struct enable_if
+{
+};
+
+template < class T > struct enable_if< true, T >
+{
+	typedef T type;
+};
+
+using std::numeric_limits;
+
+} // namespace ft
+
+#endif
